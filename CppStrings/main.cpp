39152424dfd6b82e43be1a94450cf3bd78cfba61
Pbra1484 <pbra1484@740s-pl-123.local>
2017-02-02 13:00:13 -0700

    Project Started

---
+++ CppStrings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppStrings
+//
+//  Created by Brashear, Patrick on 2/2/17.
+//  Copyright © 2017 Brashear, Patrick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
