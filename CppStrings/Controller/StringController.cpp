7e36577b07ff924f4c17809c858f910659b89db7
Pbra1484 <pbra1484@740s-pl-123.local>
2017-02-06 12:43:25 -0700

    made two arrays

--- CppStrings/Controller/StringController.cpp
+++ CppStrings/Controller/StringController.cpp
@@ -11,43 +11,61 @@
 
 using namespace std;
 
+
+
 void StringController :: start()
 {
-    aString = "stuff";
     
-    __SIZE_TYPE__ size = aString.size();
     
-    cout << "the string is " << aString << endl;
+    int ints [2];
+    double doubles [2];
+
     
-    cout << "the size is " << size << endl;
     
-    char at = aString.at(3);
     
-    cout << "the chareter at 3 is " << at << endl;
     
-    if(aString.empty())
-    {
-        cout << "the string is empty" << endl;
-    }
-    else
-    {
-        cout << "the string is not empty" << endl;
-    }
     
-    string sub = aString.substr(2, 4);
     
-    cout << "a substring of it is" << sub << endl;
     
-    if(aString.compare(sub))
-    {
-        cout << "the strings are equal" << endl;
-    }
-    else
-    {
-        cout << "the strings are not equal" << endl;
-    }
     
-    char at2 = aString[2];
     
-    cout << "the chareter at 2 is " << at2 << endl;
+    
+    
+//    aString = "stuff";
+//    
+//    __SIZE_TYPE__ size = aString.size();
+//    
+//    cout << "the string is " << aString << endl;
+//    
+//    cout << "the size is " << size << endl;
+//    
+//    char at = aString.at(3);
+//    
+//    cout << "the chareter at 3 is " << at << endl;
+//    
+//    if(aString.empty())
+//    {
+//        cout << "the string is empty" << endl;
+//    }
+//    else
+//    {
+//        cout << "the string is not empty" << endl;
+//    }
+//    
+//    string sub = aString.substr(2, 4);
+//    
+//    cout << "a substring of it is" << sub << endl;
+//    
+//    if(aString.compare(sub))
+//    {
+//        cout << "the strings are equal" << endl;
+//    }
+//    else
+//    {
+//        cout << "the strings are not equal" << endl;
+//    }
+//    
+//    char at2 = aString[2];
+//    
+//    cout << "the chareter at 2 is " << at2 << endl;
 }
